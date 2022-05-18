5c76359cf906b88d6ed1adc6334f8c404c515c4d
raviharini <89994078+raviharini@users.noreply.github.com>
2022-05-18 23:22:21 +0530

    Add files via upload

---
+++ portfolio/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+$("#burger").on("click",function(){
+$(".list").toggleClass("list-show");
+});
+
+$(".first-second").hover(function(){
+$(".first-second").toggleClass("first-second-add");
+});
+
+var typed = new Typed(".typing",{
+strings : ["web developer", "Java developer"],
+typeSpeed : 100,
+backSpeed : 60,
+loop : true
+});
+
+$(".about-click").on("click",function(){
+$(".skills,.about-me,.from").css("opacity","1");
+});
+$(window).scroll(function(){
+if(this.scrollY>550){
+$(".about-me").css("opacity","1");
+}if(this.scrollY>555){
+$(".skills,.from").css("opacity","1");
+}
+
+});
+
+});
